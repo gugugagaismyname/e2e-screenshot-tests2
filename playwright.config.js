826d23ff389011c3fd360308d84c4b904fce89c4
gugugagaismyname <romelash.69@stud.op.edu.ua>
2025-11-11 00:11:11 +0200

    Fuck

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
-    headless: true
+    headless: true,
+    viewport: { width: 1280, height: 720 },
   },
   projects: [
     {
